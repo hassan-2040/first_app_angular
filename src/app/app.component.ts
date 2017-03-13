d934162c516173cb074afcfa0a702ed94a06494c
Hassan <hassan@panteragroup.org>
2017-03-13 19:29:04 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>Jango Mango</h1>
+  <fa-other></fa-other>
+  <fa-another></fa-another>
+`,
+    // './app.component.html',
+  styles: [`h1 {
+    color: red
+  }`]
+
+// ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hola Re Amigoes!';
 }
